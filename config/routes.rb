7b9098ba3f58e4dcf474b548d0beb98f5702e7aa
brenodamata <bjrdamata@gmail.com>
2016-03-17 11:35:18 -0400

    Cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,4 @@ Rails.application.routes.draw do
   root 'prices#index'
   post '/search', to: 'prices#search'
   get '/search', to: 'prices#results'
-  # root 'salidos#brands'
-  # get '/:brand_id/locations', to: 'salidos#locations', as: 'brand_locations' 
-  # get '/:location_id/order_types', to: 'salidos#order_types', as: 'location_order_types'
-  # get '/:location_id/:order_type_id/prices', to: 'salidos#prices', as: 'location_order_type_prices'
 end
